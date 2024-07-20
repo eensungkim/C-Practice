ba9fb5af92533a6b38c1fed0993d9c8cfad7e73b
eensungkim <eensungkim@gmail.com>
2024-07-20 21:50:18 +0900

    feat: 1일차
    dotnet 프레임워크 설치, vscode 세팅
    기본적인 데이터 표현법 확인

--- Practice.cs
+++ Practice.cs
@@ -1,1 +1,27 @@
 // C# 언어 학습 1일차
+// 변수를 선언할 때는 [데이터타입] [변수명] = [저장할 데이터] 와 같은 형태
+
+
+class DataType {
+    public static void Run() {
+        int intNumber = 10; // 정수형 데이터
+        double doubleNumber = 3.14; // 실수형 데이터
+        string name = "eensungkim"; // 문자열 데이터
+        bool isPracticing = true; // 불리언 데이터
+
+        Console.WriteLine(intNumber);
+        Console.WriteLine(doubleNumber);
+        Console.WriteLine(name);
+        Console.WriteLine(isPracticing);
+    }
+}
+
+class NumberSystem {
+    public static void Run() {
+        int binaryNumber = 0b00001001;
+        int hexadecimalNumber = 0x001B;
+
+        Console.WriteLine($"0b00001001 는 십진법으로 {binaryNumber}");
+        Console.WriteLine($"0x001B 는 십진법으로 {hexadecimalNumber}");
+    }
+}
