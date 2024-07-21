12a91e2515849415175765555a5c358634298af9
eensungkim <eensungkim@gmail.com>
2024-07-21 13:48:23 +0900

    chore: 파일명 바꾸기 및 소소한 변경

--- Day1.cs
+++ Day1.cs
@@ -1,6 +1,6 @@
 // C# 언어 학습 1일차
 // 변수를 선언할 때는 [데이터타입] [변수명] = [저장할 데이터] 와 같은 형태
-
+// 문장의 마무리에는 세미콜론 ; 를 반드시 찍어주어야 함.
 
 class DataType {
     public static void Run() {
@@ -16,6 +16,8 @@ class DataType {
     }
 }
 
+// 숫자 앞에 0b 를 붙이면 2진수를 변수에 할당할 수 있고, 0x 를 붙이면 16진수를 할당할 수 있다.
+// 단 콘솔에서 확인할 때는 십진법으로 자동 변환되어 표기된다.
 class NumberSystem {
     public static void Run() {
         int binaryNumber = 0b00001001;
